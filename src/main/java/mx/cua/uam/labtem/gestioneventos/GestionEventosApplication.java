47c1a4b950038ed3ad0cc2486eb081d58fb11950
MIKE <miguel.navarro@cua.uam.mx>
2026-03-10 16:51:33 -0600

    Inicializa proyecto Spring Boot y configuración base

---
+++ src/main/java/mx/cua/uam/labtem/gestioneventos/GestionEventosApplication.java
@@ -0,0 +1,13 @@
+package mx.cua.uam.labtem.gestioneventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionEventosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionEventosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mx/cua/uam/labtem/gestioneventos/GestionEventosApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.cua.uam.labtem.gestioneventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionEventosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
